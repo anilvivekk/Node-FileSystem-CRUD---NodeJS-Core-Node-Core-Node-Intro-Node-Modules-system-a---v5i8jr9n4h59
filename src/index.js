1eaf97050242daca45891903ca4e1afbf78c0da1
anilvivekk <avanilvivek056@gmail.com>
2022-07-25 02:36:21 +0530

    fifth

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const myFileWriter = async (fileName, fileContent) => {
 
 const myFileReader = async (fileName) => {
 	try{
-		await fs.readFile(fileName, {encoding: 'utf8'})
+		await fsPromises.readFile(fileName, {encoding: 'utf8'})
 	}catch(err){
 		console.log(err)
 	}
