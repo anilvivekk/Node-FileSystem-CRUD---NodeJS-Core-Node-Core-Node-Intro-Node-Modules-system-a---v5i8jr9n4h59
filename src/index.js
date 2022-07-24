634beb4b52c257eee6d0e8f969e2c27d7c098fe8
anilvivekk <avanilvivek056@gmail.com>
2022-07-25 02:01:00 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 const fs = require('fs/promises')
 
 const myFileWriter = async (fileName, fileContent) => {
+	try {
+		await fs.writeFile('fileName', fileContent)
+	}catch (err){
+		console.log(err);
+	}
+	
 	// write code here
 	// dont chnage function name
 }
 
 const myFileReader = async (fileName) => {
+	await fs.readFile('fileName')
 	// write code here
 	// dont chnage function name
 }
 
 
 const myFileUpdater = async (fileName, fileContent) => {
+	await fs.appendFile('fileName', fileContent);
 	// write code here
 	// dont chnage function name
 }
 
 const myFileDeleter = async (fileName) => {
+	await fs.rmdir('fileName')
 	// write code here
 	// dont chnage function name
 }
